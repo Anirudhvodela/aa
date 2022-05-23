964c9ae3e28b275439627acd26bf37bef5ebc41f
Anirudh <anirudhvodela@gmail.com>
2022-05-23 17:26:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-     aa
-    </div>
+    <Navbar />
   );
 }
 
